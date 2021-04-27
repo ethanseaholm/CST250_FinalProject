3484d1e51e5021ba6d821ae3e28564bfb26f8e65
Ethan Seaholm <ethanseaholm@gmail.com>
2021-04-27 17:44:51 -0500

    Final Project Progress

---
+++ src/main/java/com/mycompany/finalproject/MainClass.java
@@ -0,0 +1,36 @@
+package com.mycompany.finalproject;
+import java.util.Scanner;
+import java.util.Random;
+
+/*
+
+Requirements:
+
+- The ability to create instances of the Player objects with their proper info (20%)
+
+- The ability to randomly generate a trick for a player (15%)
+
+- The ability to randomly decide if the trick was landed successfully for a player (15%)
+
+- The ability to show the current points (letters each player has got) (20%)
+
+- The ability to store the current points of each player and both players info on disk and in a file (30%)
+
+BONUS (50 points each):
+
+- Support for three players: two offenders and one defender. A letter is assigned the trick is landed by any of the offenders 
+after missed by the defender
+
+- Some sort of a graphical interface
+
+*/
+
+public class MainClass {
+    
+    public static void main(String[] args){
+        
+        System.out.println("---");
+        
+    }
+    
+}
